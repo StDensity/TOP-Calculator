751bf4c4570a32a670009df435eec3fa037716f1
StDestiny <84127029+StDensity@users.noreply.github.com>
2025-09-15 19:11:17 +0200

    Add basic function calling logic

--- script.js
+++ script.js
@@ -1,15 +1,34 @@
-const add = (a, b) => {
-    return a + b
-}
+const add = (num1, num2) => {
+   return num1 + num2;
+};
 
-const subtract = (a, b) => {
-    return a - b
-}
+const subtract = (num1, num2) => {
+   return num1 - num2;
+};
 
-const multiply = (a, b) => {
-    return a * b
-}
+const multiply = (num1, num2) => {
+   return num1 * num2;
+};
 
-const divide = (a, b) => {
-    return a / b
-}
+const divide = (num1, num2) => {
+   return num1 / num2;
+};
+
+const operate = (num1, num2, operator) => {
+   if (operator === "+") {
+      return add(num1, num2);
+   } else if (operate === "-") {
+      return subtract(num1, num2);
+   } else if (operator === "*") {
+      return multiply(num1, num2);
+   } else if (operator === "/") {
+      return divide(num1, num2);
+   }
+};
+
+
+let num1;
+let num2;
+let operator;
+
+// GUI
