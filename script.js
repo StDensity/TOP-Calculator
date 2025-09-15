e7ad43e6c65ae85a8d2fc5eed51d88d065941f91
StDestiny <84127029+StDensity@users.noreply.github.com>
2025-09-16 00:22:51 +0200

    Implement basic calculator

--- script.js
+++ script.js
@@ -14,23 +14,158 @@ const divide = (num1, num2) => {
    return num1 / num2;
 };
 
-const operate = (num1, num2, operator) => {
+const operate = (a, b, operator) => {
+   console.log(a, b, operator);
    if (operator === "+") {
-      return add(num1, num2);
-   } else if (operate === "-") {
-      return subtract(num1, num2);
+      console.log("hi");
+      num1 = add(a, b);
+   } else if (operator === "-") {
+      console.log("subtraction", num1);
+      num1 = subtract(a, b);
    } else if (operator === "*") {
-      return multiply(num1, num2);
+      num1 = multiply(a, b);
    } else if (operator === "/") {
-      return divide(num1, num2);
+      if (b === 0) {
+         num1 = "Division by 0 is not possible";
+      } else {
+         num1 = divide(a, b);
+      }
+   }
+   setDisplayValue(num1);
+   //    operator === null;
+   num2 = 0;
+};
+
+const handleSpecialButtons = (op) => {
+   if (op === "clear") {
+      num1 = 0;
+      num2 = 0;
+      operator = null;
+      setDisplayValue("Hello");
+   } else if (op === "=") {
+      operate(num1, num2, operator);
+      operator = null;
    }
 };
 
-let num1;
-let num2;
-let operator;
+const setDisplayValue = (str) => {
+   if (!isNaN(parseInt(str))) {
+      str = Math.round(str * 100000) / 100000;
+   }
+   const displayArea = document.querySelector("#display");
+   displayArea.textContent = str;
+};
+
+const handleNumberButtonPress = (num) => {
+   if (operator === null) {
+      num1 = num1 * 10 + num;
+      setDisplayValue(num1);
+   } else {
+      num2 = num2 * 10 + num;
+      setDisplayValue(num2);
+   }
+};
+
+const handleOperatorButtonPress = (op) => {
+   if (operator === null) {
+      operator = op;
+      setDisplayValue(operator);
+   } else {
+      operate(num1, num2, operator);
+      operator = op;
+      //   setDisplayValue(operator)
+   }
+};
+
+const OPERATORS = "+-*/";
+let num1 = 0;
+let num2 = 0;
+let operator = null;
 let displayValue = "Welcome";
+
 // GUI
 
 const displayArea = document.querySelector("#display");
 displayArea.textContent = displayValue;
+
+// Buttons
+
+const one = document.querySelector("#number1");
+one.addEventListener("click", () => {
+   handleNumberButtonPress(1);
+});
+
+const two = document.querySelector("#number2");
+two.addEventListener("click", () => {
+   handleNumberButtonPress(2);
+});
+
+const three = document.querySelector("#number3");
+three.addEventListener("click", () => {
+   handleNumberButtonPress(3);
+});
+
+const four = document.querySelector("#number4");
+four.addEventListener("click", () => {
+   handleNumberButtonPress(4);
+});
+
+const five = document.querySelector("#number5");
+five.addEventListener("click", () => {
+   handleNumberButtonPress(5);
+});
+
+const six = document.querySelector("#number6");
+six.addEventListener("click", () => {
+   handleNumberButtonPress(6);
+});
+
+const seven = document.querySelector("#number7");
+seven.addEventListener("click", () => {
+   handleNumberButtonPress(7);
+});
+
+const eight = document.querySelector("#number8");
+eight.addEventListener("click", () => {
+   handleNumberButtonPress(8);
+});
+
+const nine = document.querySelector("#number9");
+nine.addEventListener("click", () => {
+   handleNumberButtonPress(9);
+});
+
+const zero = document.querySelector("#number0");
+zero.addEventListener("click", () => {
+   handleNumberButtonPress(0);
+});
+
+const clear = document.querySelector("#clear");
+clear.addEventListener("click", () => {
+   handleSpecialButtons("clear");
+});
+
+const equal = document.querySelector("#equal");
+equal.addEventListener("click", () => {
+   handleSpecialButtons("=");
+});
+
+const addButton = document.querySelector("#sum");
+addButton.addEventListener("click", () => {
+   handleOperatorButtonPress("+");
+});
+
+const subtractButton = document.querySelector("#subtract");
+subtractButton.addEventListener("click", () => {
+   handleOperatorButtonPress("-");
+});
+
+const multiplyButton = document.querySelector("#multiply");
+multiplyButton.addEventListener("click", () => {
+   handleOperatorButtonPress("*");
+});
+
+const DivideButton = document.querySelector("#divide");
+DivideButton.addEventListener("click", () => {
+   handleOperatorButtonPress("/");
+});
